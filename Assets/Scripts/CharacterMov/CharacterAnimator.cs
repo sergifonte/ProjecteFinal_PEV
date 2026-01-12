6095435e1e34050fd43a30fd5a2de960eaa856d4
sergifonte <sergi.font.escoin@estudiantat.upc.edu>
2026-01-12 20:17:05 +0100

    done

--- Assets/Scripts/CharacterMov/CharacterAnimator.cs
+++ Assets/Scripts/CharacterMov/CharacterAnimator.cs
@@ -3,38 +3,32 @@ using UnityEngine;
 public class CharacterAnimator : MonoBehaviour
 {
     private Animator _animator;
+    private CharacterMovement _movement;
     private InputHandler _input;
 
-    void Awake()
-    {
+    void Awake() {
         _animator = GetComponent<Animator>();
         _input = GetComponent<InputHandler>();
+        _movement = GetComponent<CharacterMovement>();
     }
 
-    private void OnEnable()
-    {
-        
-        WorldState.OnWorldStateChanged += ActualizarParametroMundo;
-        
-        if (WorldState.Instance != null) ActualizarParametroMundo(WorldState.Instance.state);
-    }
+    private void OnEnable() => WorldState.OnWorldStateChanged += ActualizarEstadoVisual;
+    private void OnDisable() => WorldState.OnWorldStateChanged -= ActualizarEstadoVisual;
 
-    private void OnDisable()
-    {
-        WorldState.OnWorldStateChanged -= ActualizarParametroMundo;
-    }
+    void Update() {
+        if (_animator == null) return;
 
-    void Update()
-    {
-        float speed = _input.GetInputInHorizontalPlane().magnitude;
-        _animator.SetFloat("Speed", speed);
-    }
+        _animator.SetFloat("Speed", _input.MoveInput.magnitude * 0.1f);
 
-    private void ActualizarParametroMundo(float nuevoEstado)
-    {
-        if (_animator != null)
-        {
-            _animator.SetFloat("State", nuevoEstado);
+        bool aTerra = !_movement.EstaEnAire();
+        _animator.SetBool("isGrounded", aTerra);
+
+        if (aTerra && Input.GetButtonDown("Jump")) {
+            _animator.SetTrigger("Jump");
         }
+    }
+
+    private void ActualizarEstadoVisual(float nuevoEstado) {
+        _animator.SetFloat("State", nuevoEstado);
     }
 }
--- Assets/Scripts/CharacterMov/CharacterMovement.cs
+++ Assets/Scripts/CharacterMov/CharacterMovement.cs
@@ -2,102 +2,84 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [Header("ajustes utopía (estado 1)")]
+    [Header("Ajustos de Salt")]
+    public float tempsAnticipacio = 0.6f; 
+    private bool preparantSalt = false;
+
+    [Header("Ajustos de Moviment")]
     public float SpeedUtopia = 8f;
     public float GravityUtopia = -9.81f;
     public float JumpUtopia = 12f;
-
-    [Header("ajustes distopía (estado -1)")]
     public float SpeedDistopia = 4f;
     public float GravityDistopia = -35f;
     public float JumpDistopia = 4f;
 
-    [Header("variables actuales (calculadas por evento)")]
-    public float RotationSpeed = 0.5f;
-    private float currentSpeed;
-    private float currentGravity;
-    private float currentJumpForce;
-
+    private float currentSpeed, currentGravity, currentJumpForce;
     private CharacterController _controller;
     private InputHandler _input;
     private float _verticalVelocity;
+    public float RotationSpeed = 0.5f;
 
-    void Awake()
-    {
+    void Awake() {
         _controller = GetComponent<CharacterController>();
         _input = GetComponent<InputHandler>();
     }
 
-    void Start()
-    {
-        if (WorldState.Instance != null)
-        {
-            ActualizarParametrosFisicos(WorldState.Instance.state);
-        }
+    void Start() {
+        if (WorldState.Instance != null) ActualizarParametrosFisicos(WorldState.Instance.state);
     }
 
-    private void OnEnable()
-    {
-        WorldState.OnWorldStateChanged += ActualizarParametrosFisicos;
-    }
+    private void OnEnable() => WorldState.OnWorldStateChanged += ActualizarParametrosFisicos;
+    private void OnDisable() => WorldState.OnWorldStateChanged -= ActualizarParametrosFisicos;
 
-    private void OnDisable()
-    {
-        WorldState.OnWorldStateChanged -= ActualizarParametrosFisicos;
-    }
-
-    void Update()
-    {
+    void Update() {
         Rotate();
         ApplyGravity();
         CheckJump();
         Move();
     }
 
-    private void ActualizarParametrosFisicos(float estadoActual)
-    {
+    private void CheckJump() {
+        if (_controller.isGrounded && Input.GetButtonDown("Jump") && !preparantSalt) {
+            preparantSalt = true;
+            Invoke("ExecutarSaltFisic", tempsAnticipacio);
+        }
+    }
+
+    private void ExecutarSaltFisic() {
+        _verticalVelocity = currentJumpForce;
+        Invoke("FinalitzarGuardaSalt", 0.2f);
+    }
+
+    private void FinalitzarGuardaSalt() {
+        preparantSalt = false;
+    }
+
+    public bool EstaEnAire() {
+        return !_controller.isGrounded || preparantSalt || _verticalVelocity > 0;
+    }
+
+    private void ActualizarParametrosFisicos(float estadoActual) {
         float t = (estadoActual + 1f) / 2f;
         currentSpeed = Mathf.Lerp(SpeedDistopia, SpeedUtopia, t);
         currentGravity = Mathf.Lerp(GravityDistopia, GravityUtopia, t);
         currentJumpForce = Mathf.Lerp(JumpDistopia, JumpUtopia, t);
     }
 
-    private void Move()
-    {
-       
+    private void Move() {
         Vector3 moveDirection = transform.forward * _input.MoveInput.y;
         Vector3 velocity = moveDirection * currentSpeed;
-
         velocity.y = _verticalVelocity;
         _controller.Move(velocity * Time.deltaTime);
     }
 
-    private void ApplyGravity()
-    {
-        if (_controller.isGrounded)
-        {
-            if (_verticalVelocity < 0) _verticalVelocity = -2f;
-        }
-        else
-        {
-            _verticalVelocity += currentGravity * Time.deltaTime;
-        }
+    private void ApplyGravity() {
+        if (_controller.isGrounded && _verticalVelocity < 0) _verticalVelocity = -2f;
+        else _verticalVelocity += currentGravity * Time.deltaTime;
     }
 
-    private void CheckJump()
-    {
-        if (_controller.isGrounded && Input.GetButtonDown("Jump"))
-        {
-            _verticalVelocity = currentJumpForce;
-        }
-    }
-
-    private void Rotate()
-    {
+    private void Rotate() {
         float rotationInput = _input.MoveInput.x;
-        if (rotationInput != 0)
-        {
-            transform.Rotate(Vector3.up, rotationInput * RotationSpeed);
-        }
+        if (rotationInput != 0) transform.Rotate(Vector3.up, rotationInput * RotationSpeed);
     }
 }
